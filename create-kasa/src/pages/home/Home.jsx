d18f6daf8ec3ae77e174d6eb1aa0a2905e99ebb4
dylancatala <dylan.catala@proton.me>
2023-03-28 19:37:14 +0200

    Refactoring - Adding components

--- create-kasa/src/pages/home/Home.jsx
+++ create-kasa/src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/header/Header';
+
 
 const Home = () => {
   return (
     <div className='page-wrapper'>
       <Header title="Chez vous, partout et ailleurs" imageURL="images/home-banner.png" />
-    </div>
 
+    </div>
   );
 };
 
