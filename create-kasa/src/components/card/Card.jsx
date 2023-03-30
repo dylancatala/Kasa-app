04269376f838d312017487337d89128adc1bab8f
dylancatala <dylan.catala@proton.me>
2023-03-30 20:05:51 +0200

    Adding components to home page/Adding data json

---
+++ create-kasa/src/components/card/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+  return (
+    <div className='card-template'>
+    </div>
+  );
+};
+
+export default Card;
--- create-kasa/src/pages/home/Home.jsx
+++ create-kasa/src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import './Home.css';
+import Card from '../../components/card/Card';
 import Header from '../../components/header/Header';
+import dataHouse from '../../data/logements.json';
 
 
 const Home = () => {
   return (
     <div className='page-wrapper'>
       <Header title="Chez vous, partout et ailleurs" imageURL="images/home-banner.png" />
-
+      <div className="grid-template">
+      {dataHouse.map((items, index) => (
+          <Card key={index} image={items.cover} />
+        ))}
+      </div>
     </div>
   );
 };
